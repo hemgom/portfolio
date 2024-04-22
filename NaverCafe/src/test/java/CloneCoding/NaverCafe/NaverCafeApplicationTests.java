49ea63029663ecd484cfa95945c44e63ad82ab77
hemgom <reborn.april23@gmail.com>
2024-04-22 18:17:26 +0900

    create project NaverCafe

---
+++ NaverCafe/src/test/java/CloneCoding/NaverCafe/NaverCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package CloneCoding.NaverCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
