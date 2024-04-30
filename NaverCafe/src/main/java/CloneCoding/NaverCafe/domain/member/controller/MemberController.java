09a9abf92c3dd0f064b04698419d9981af555464
hemgom <reborn.april23@gmail.com>
2024-04-30 22:03:58 +0900

    add join member info constraints

--- NaverCafe/src/main/java/CloneCoding/NaverCafe/domain/member/controller/MemberController.java
+++ NaverCafe/src/main/java/CloneCoding/NaverCafe/domain/member/controller/MemberController.java
@@ -2,12 +2,15 @@ package CloneCoding.NaverCafe.domain.member.controller;
 
 import CloneCoding.NaverCafe.domain.member.dto.RequestJoinMember;
 import CloneCoding.NaverCafe.domain.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
@@ -16,7 +19,8 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/join")
-    public String joinMember(@RequestBody RequestJoinMember request) {
+    public String joinMember(@RequestBody @Valid RequestJoinMember request) {
+        log.info("회원 등록 요청");
         return memberService.joinMember(request);
     }
 
--- NaverCafe/src/main/java/CloneCoding/NaverCafe/domain/member/dto/RequestJoinMember.java
+++ NaverCafe/src/main/java/CloneCoding/NaverCafe/domain/member/dto/RequestJoinMember.java
@@ -1,5 +1,6 @@
 package CloneCoding.NaverCafe.domain.member.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,22 +12,45 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RequestJoinMember {
 
+    @NotNull
+    @Size(min = 5, max = 20)
+    @Pattern(regexp = "^[a-z0-9_-]+")
     private String accountId;
 
+    @NotNull
+    @Size(min = 8, max = 16)
+    @Pattern(regexp = "^[a-zA-Z0-9~!@#$%]+", message = "비밀번호 정규식 오류")
     private String accountPassword;
 
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z0-9_]+@[a-zA-Z]+\\.[a-zA-Z]+(\\.[a-zA-Z]+)?")
     private String email;
 
+    @NotNull
+    @Size(min = 2, max = 18)
+    @Pattern(regexp = "^[a-zA-Z]+")
     private String username;
 
+    @NotNull
+    @Min(value = 1924) @Max(value = 2024)
     private int year;
 
+    @NotNull
+    @Min(value = 1) @Max(value = 12)
     private int month;
 
+    @NotNull
+    @Min(value = 1) @Max(value = 31)
     private int day;
 
+    @NotNull
+    @Size(min = 11, max = 11)
+    @Pattern(regexp = "^[0-9]+")
     private String phoneNumber;
 
+    @NotNull
+    @Size(min = 2, max = 20)
+    @Pattern(regexp = "^[a-zA-Z0-9]+")
     private String nickname;
 
 }
